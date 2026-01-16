9df0ed4c0cb6a85e0a57ebbb8a7406fc617c5ab5
IgnacioOchoa <onacho87@gmail.com>
2026-01-17 00:21:50 +0100

    Agregar modelo 3D desde el front

--- wayralanding-frontend/src/three/init.js
+++ wayralanding-frontend/src/three/init.js
@@ -1,9 +1,11 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import modelUrl from "./low_poly_airplane.glb";
 
 export function initThree(container) {
   // Scene
   const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x222222);
+  scene.background = new THREE.Color(0x121212);
 
   // Camera
   const camera = new THREE.PerspectiveCamera(
@@ -12,7 +14,8 @@ export function initThree(container) {
     0.1,
     1000
   );
-  camera.position.z = 2;
+  camera.position.z = 100;
+  camera.position.y = 30;
 
   // Renderer
   const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -20,15 +23,38 @@ export function initThree(container) {
   container.appendChild(renderer.domElement);
 
   // Geometry
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshNormalMaterial();
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+//   const geometry = new THREE.BoxGeometry();
+//   const material = new THREE.MeshNormalMaterial();
+//   const cube = new THREE.Mesh(geometry, material);
+//   scene.add(cube);
+
+// Light (GLB models NEED light)
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(5, 10, 5);
+scene.add(light);
+scene.add(new THREE.AmbientLight(0xffffff, 0.5));
+
+// Load model
+const loader = new GLTFLoader();
+let model = null;
+loader.load(
+  modelUrl,
+  (gltf) => {
+    model = gltf.scene;
+    scene.add(model);
+  },
+  undefined,
+  (error) => {
+    console.error("Error loading GLB:", error);
+  }
+);
+
 
   // Render loop
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    if (model) {
+        model.rotation.y += 0.01;
+      }
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
   }
--- wayralanding-frontend/vite.config.js
+++ wayralanding-frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  assetsInclude: ["**/*.glb"],
   server: {
     origin: "http://localhost:5173",
     strictPort: true,
