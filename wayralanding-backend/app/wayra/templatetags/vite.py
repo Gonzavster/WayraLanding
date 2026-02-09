8e0d2b8d8e73df166a3bead5be5ed5cd4683066b
IgnacioOchoa <onacho87@gmail.com>
2026-02-09 23:41:22 +0100

    Mejorar carga desde vite_asset y agregar logging

--- wayralanding-backend/app/wayra/templatetags/vite.py
+++ wayralanding-backend/app/wayra/templatetags/vite.py
@@ -4,15 +4,23 @@ import json
 from pathlib import Path
 
 register = template.Library()
-
+logger = logging.getLogger(__name__)
+# Text is logged to /var/log/gunicorn/gunicorn.err.log, run it with tail -f <path>
 
 @register.simple_tag
 def vite_asset(entry):
+    #logger.error(f"VITE: mode={'DEBUG' if settings.DEBUG else 'PROD'} entry={entry}")
     if settings.DEBUG:
+        #logger.error("It is getting into DEBUG")
         return f"http://localhost:5173/src/{entry}"
 
+    #logger.error(f"STATIC_ROOT = {settings.STATIC_ROOT}")
     manifest_path = settings.STATIC_ROOT / "manifest.json"
     with open(manifest_path) as f:
         manifest = json.load(f)
-
-    return settings.STATIC_URL + manifest[entry]["file"]
+    #logger.error("VITE MANIFEST:\n%s", json.dumps(manifest, indent=2))
+    if entry not in manifest:
+        raise Exception(f"{entry} not in manifest keys: {manifest.keys()}")
+    res=  settings.STATIC_URL + manifest[entry]["file"]
+    #logger.error(f"result = {res}")
+    return res
