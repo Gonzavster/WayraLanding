93a7ea917b402c3402a9f645af25e0aa7b56cdb3
IgnacioOchoa <onacho87@gmail.com>
2026-02-09 23:42:18 +0100

    Robustecer la ruta de carga de los assets

--- wayralanding-frontend/src/three/init.js
+++ wayralanding-frontend/src/three/init.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
-import modelUrl from "./low_poly_airplane.glb";
+import modelUrl from "./low_poly_airplane.glb?url";
 
 export function initThree(container) {
   // Scene
